c93ae87e096941e58da4764c3e7b5500240cfbcc
Manuel Arce <manuel_040192@hotmail.com>
2022-09-06 19:41:33 +0200

    first commit

---
+++ src/AddressData.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import erc20abi from "./erc20ABI.json";
+import { ethers } from 'ethers';
+
+const AddressData = () => {
+
+  const INFURA_ID = '790a3c8a86b6456497d4bf32fc97c0eb';
+
+  const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${INFURA_ID}`);
+
+  const address = '0x6B175474E89094C44Da98b954EedeAC495271d0F'
+
+  const [addressName, setAddressName] = useState(null);
+  
+  const [addressSymbol, setAddressSymbol] = useState(null);
+
+  const getAddressData = async () => {
+    const contract = new ethers.Contract(address, erc20abi, provider);
+    const tokenName = await contract.name();
+    const tokenSymbol = await contract.symbol();
+    setAddressName(tokenName);
+    setAddressSymbol(tokenSymbol);
+  }
+
+  getAddressData();
+
+  return (
+    <div>
+      <div>Token name: {addressName}</div>
+      <div>Token symbol: {addressSymbol}</div>
+    </div>
+  );
+}
+
+export default AddressData;
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import AddressData from './AddressData';
+
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <AddressData />
+    </div>
+  );
+}
+
+export default App;
